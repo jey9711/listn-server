095f6654c028602217af81003d08ba026b36791f
Jeremy Hidajat <jhidajat@uwaterloo.ca>
2018-04-18 06:40:02 -0400

    removed alert

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ app.get('/callback', function(req, res) {
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    alert(uri);
     res.redirect(uri + '?access_token=' + access_token)
   })
 })

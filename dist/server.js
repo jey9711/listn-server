18e4959e2213bf877e10e0eb617c0408b0a38129
Jeremy Hidajat <jhidajat@uwaterloo.ca>
2018-04-19 18:35:10 -0400

    factor out env variables

--- dist/server.js
+++ dist/server.js
@@ -1,19 +1,33 @@
 'use strict';
 
-var express = require('express');
-var request = require('request');
-var querystring = require('querystring');
+var _express = require('express');
 
-var app = express();
+var _express2 = _interopRequireDefault(_express);
 
-var redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _querystring = require('querystring');
+
+var _querystring2 = _interopRequireDefault(_querystring);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+var CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+var REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback';
+var FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000';
+var PORT = process.env.PORT || '8888';
+
+var app = (0, _express2.default)();
 
 app.get('/login', function (req, res) {
-  res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
+  res.redirect('https://accounts.spotify.com/authorize?' + _querystring2.default.stringify({
     response_type: 'code',
-    client_id: process.env.SPOTIFY_CLIENT_ID,
+    client_id: CLIENT_ID,
     scope: 'user-read-private user-read-email user-read-playback-state',
-    redirect_uri: redirect_uri
+    redirect_uri: REDIRECT_URI
   }));
 });
 
@@ -23,21 +37,19 @@ app.get('/callback', function (req, res) {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
-      redirect_uri: redirect_uri,
+      redirect_uri: REDIRECT_URI,
       grant_type: 'authorization_code'
     },
     headers: {
-      'Authorization': 'Basic ' + new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+      'Authorization': 'Basic ' + new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
     },
     json: true
   };
-  request.post(authOptions, function (error, response, body) {
+  _request2.default.post(authOptions, function (error, response, body) {
     var access_token = body.access_token;
-    var uri = process.env.FRONTEND_URI || 'http://localhost:3000';
-    res.redirect(uri + '?access_token=' + access_token);
+    res.redirect(FRONTEND_URI + '?access_token=' + access_token);
   });
 });
 
-var port = process.env.PORT || 8888;
-console.log('Listening on port ' + port + '. Go /login to initiate authentication flow.');
-app.listen(port);
+console.log('Listening on port ' + PORT + '. Go /login to initiate authentication flow.');
+app.listen(PORT);
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-let express = require('express')
-let request = require('request')
-let querystring = require('querystring')
+import express from 'express'
+import request from 'request'
+import querystring from 'querystring'
 
-let app = express()
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:8888/callback'
+const FRONTEND_URI = process.env.FRONTEND_URI || 'http://localhost:3000'
+const PORT = process.env.PORT || '8888'
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+const app = express()
 
 app.get('/login', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_id: CLIENT_ID,
       scope: 'user-read-private user-read-email user-read-playback-state',
-      redirect_uri
+      redirect_uri: REDIRECT_URI,
     }))
 })
 
@@ -24,23 +26,21 @@ app.get('/callback', function(req, res) {
     url: 'https://accounts.spotify.com/api/token',
     form: {
       code: code,
-      redirect_uri,
+      redirect_uri: REDIRECT_URI,
       grant_type: 'authorization_code'
     },
     headers: {
       'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+        CLIENT_ID + ':' + CLIENT_SECRET
       ).toString('base64'))
     },
     json: true
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
+    res.redirect(FRONTEND_URI + '?access_token=' + access_token)
   })
 })
 
-let port = process.env.PORT || 8888
-console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
-app.listen(port)
+console.log(`Listening on port ${PORT}. Go /login to initiate authentication flow.`)
+app.listen(PORT)
